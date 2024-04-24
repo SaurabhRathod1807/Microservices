cc65e547afc8d82f85890a01613885892e817559
Saurabh Rathod <Saurabh Rathod@192.168.142.1>
2024-04-24 23:02:54 +0530

    FirstCommite

---
+++ Institute-service/src/test/java/com/institutes/InstituteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
